ebba20f94c956cdb57868b683a911a4ff00ef4a5
Tim Gretler <gretler.tim@gmail.com>
2020-03-30 17:53:33 +0200

    imporved map file laylout

--- flask_webapp/static/js/map.js
+++ flask_webapp/static/js/map.js
@@ -48,9 +48,8 @@ function create_map() {
     marker.tooltipText = "{title}";
     marker.horizontalCenter = "middle";
     marker.verticalCenter = "middle";
-    console.log(podinfo);
+
     if (podinfo["success"] == true) {
-      console.log(podinfo["labels"]);
       if (podinfo["labels"]["cloud"] == "gcp") {
         marker.href = "static/images/gcp.svg";
       } else if (podinfo["labels"]["cloud"] == "aws") {
@@ -77,6 +76,7 @@ function create_map() {
     imageSeriesTemplate.verticalCenter = "middle";
     imageSeriesTemplate.align = "center";
     imageSeriesTemplate.valign = "middle";
+    imageSeries.gr;
     imageSeriesTemplate.width = 8;
     imageSeriesTemplate.height = 8;
     imageSeriesTemplate.nonScaling = true;
@@ -89,12 +89,26 @@ function create_map() {
 
     //logic to add location of origin of request depends on label provided by podinfo api
     // the identification is based on the region of the region provided by the cloud providers
+    loc = [get_location(podinfo)];
 
-    loc = get_location(podinfo);
-    console.log(loc);
-    imageSeries.data = [loc];
+    //load prvious locations of webpage
+    try {
+      var prev_loc = localStorage.getItem("prev_loc");
+      JSON.parse(prev_loc);
+    } catch (e) {
+      prev_loc = [];
+    }
 
-    console.log(imageSeries.data);
+    //add new loaction to prevous locations
+    locs = loc.concat(prev_loc);
+    console.log(locs);
+
+    imageSeries.data = [locs[0]];
+
+    localStorage.setItem("prev_loc", JSON.stringify(locs));
+    console.log(localStorage);
+
+    //console.log(imageSeries.data);
   }); // end am4core.ready()
 }
 
@@ -102,27 +116,21 @@ function get_location(podinfo) {
   if (podinfo["success"] == true) {
     if (podinfo["labels"]["cloud"] == "gcp") {
       for (const loc of gcp_loc) {
-        console.log(podinfo["labels"]["region"]);
-        console.log(loc["region"]);
+        //console.log(podinfo["labels"]["region"]);
+        //console.log(loc["region"]);
         if (loc["region"] == podinfo["labels"]["region"]) {
-          console.log("GCP");
-          console.log(loc);
           return loc;
         }
       }
     } else if (podinfo["labels"]["cloud"] == "aws") {
       for (const loc of aws_loc) {
         if (loc["region"] == podinfo["labels"]["region"]) {
-          console.log("AWS");
-          console.log(loc);
           return loc;
         }
       }
     } else if (podinfo["labels"]["cloud"] == "azure") {
       for (const loc of azure_loc) {
         if (loc["region"] == podinfo["labels"]["region"]) {
-          console.log("AZUE");
-          console.log(loc);
           return loc;
         }
       }
@@ -133,7 +141,6 @@ function get_location(podinfo) {
           latitude: 25,
           longitude: 0.1
         };
-        console.log("UNKNOWN");
         return loc;
       } else {
         loc = {
@@ -145,7 +152,6 @@ function get_location(podinfo) {
             podinfo["pub_ip_info"]["location"].split(",")[1]
           )
         };
-        console.log("SOMEHWERE");
         return loc;
       }
     }
@@ -159,374 +165,6 @@ function get_location(podinfo) {
   }
 }
 
-aws_loc = [
-  {
-    title: "Frankfurt",
-    latitude: 50.110924,
-    longitude: 8.68212,
-    region: "eu-central-1"
-  },
-  {
-    title: "Paris",
-    latitude: 48.864716,
-    longitude: 2.349014,
-    region: "eu-west-3"
-  },
-  {
-    title: "Stockholm",
-    latitude: 59.334591,
-    longitude: 18.06324,
-    region: "eu-north-1"
-  },
-  {
-    title: "London",
-    latitude: 51.509865,
-    longitude: -0.118092,
-    region: "eu-west-2"
-  },
-  {
-    title: "Ireland",
-    latitude: 53.35014,
-    longitude: -6.266155,
-    region: "eu-west-1"
-  },
-  {
-    title: "Sao Paulo",
-    latitude: -23.533773,
-    longitude: -46.62529,
-    region: "sa-east-1"
-  },
-  {
-    title: "Oregon",
-    latitude: 45.523064,
-    longitude: -122.676483,
-    region: "us-west-2"
-  },
-  {
-    title: "North California",
-    latitude: 37.733795,
-    longitude: -122.446747,
-    region: "us-west-1"
-  },
-  {
-    title: "Ohio",
-    latitude: 39.983334,
-    longitude: -82.98333,
-    region: "us-east-2"
-  },
-  {
-    title: "Canada Central",
-    latitude: 45.508888,
-    longitude: -73.561668,
-    region: "ca-central-1"
-  },
-  {
-    title: "North Verginia",
-    latitude: 39.01405,
-    longitude: -77.065674,
-    region: "us-east-1"
-  },
-  {
-    title: "Bahrain",
-    latitude: 26.156258,
-    longitude: 50.400505,
-    region: "me-south-1"
-  },
-  {
-    title: "Mumbai",
-    latitude: 19.07609,
-    longitude: 72.877426,
-    region: "ap-south-1"
-  },
-  {
-    title: "Singapore",
-    latitude: 1.29027,
-    longitude: 103.851959,
-    region: "ap-southeast-1"
-  },
-  {
-    title: "Sydney",
-    latitude: -33.865143,
-    longitude: 151.2099,
-    region: "ap-southeast-2"
-  },
-  {
-    title: "Hong Kong",
-    latitude: 22.302219,
-    longitude: 114.174637,
-    region: "ap-east-1"
-  },
-  {
-    title: "Seoul",
-    latitude: 37.5326,
-    longitude: 127.024612,
-    region: "ap-northeast-2"
-  },
-  {
-    title: "Tokyo",
-    latitude: 35.652832,
-    longitude: 139.839478,
-    region: "ap-northeast-1"
-  }
-];
-
-// google datacenter
-gcp_loc = [
-  {
-    title: "Frankfurt",
-    latitude: 50.110924,
-    longitude: 8.682127,
-    region: "europe-west3"
-  },
-  {
-    title: "Belgium",
-    latitude: 50.8504486,
-    longitude: 4.3487802,
-    region: "europe-west1"
-  },
-  {
-    title: "Finland",
-    latitude: 60.165249,
-    longitude: 24.936056,
-    region: "europe-north1"
-  },
-  {
-    title: "London",
-    latitude: 51.509865,
-    longitude: -0.118092,
-    region: "europe-west2"
-  },
-  {
-    title: "Zurich",
-    latitude: 47.36667,
-    longitude: 8.55,
-    region: "europe-west6"
-  },
-  {
-    title: "Netherlands",
-    latitude: 52.314224,
-    longitude: 4.941841,
-    region: "europe-west4"
-  },
-  {
-    title: "Sao Paulo",
-    latitude: -23.533773,
-    longitude: -46.62529,
-    region: "southamerica-east1"
-  },
-  {
-    title: "Oregon",
-    latitude: 45.523064,
-    longitude: -122.676483,
-    region: "us-west1"
-  },
-  {
-    title: "Los Angeles",
-    latitude: 34.052235,
-    longitude: -118.243683,
-    region: "us-west2"
-  },
-  {
-    title: "Salt Lake City",
-    latitude: 40.746445,
-    longitude: -111.874916,
-    region: "us-west3"
-  },
-  {
-    title: "Iowa",
-    latitude: 41.619549,
-    longitude: -93.598022,
-    region: "us-central1"
-  },
-  {
-    title: "Monetral",
-    latitude: 45.508888,
-    longitude: -73.561668,
-    region: "northamerica-northeast1"
-  },
-  {
-    title: "North Virginia",
-    latitude: 39.01405,
-    longitude: -77.065674,
-    region: "us-east4"
-  },
-  {
-    title: "South Corolina",
-    latitude: 34.0003,
-    longitude: -81.0398,
-    region: "us-east1"
-  },
-  {
-    title: "Taiwan",
-    latitude: 25.105497,
-    longitude: 121.597366,
-    region: "asia-east1"
-  },
-  {
-    title: "Mumbai",
-    latitude: 19.07609,
-    longitude: 72.877426,
-    region: "asia-south1"
-  },
-  {
-    title: "Singapore",
-    latitude: 1.29027,
-    longitude: 103.851959,
-    region: "asia-southeast1"
-  },
-  {
-    title: "Sydney",
-    latitude: -33.865143,
-    longitude: 151.2099,
-    region: "australia-southeast1"
-  },
-  {
-    title: "Hong Kong",
-    latitude: 22.302219,
-    longitude: 114.174637,
-    region: "asia-east2"
-  },
-  {
-    title: "Osaka",
-    latitude: 34.669529,
-    longitude: 135.497009,
-    region: "asia-northeast2"
-  },
-  {
-    title: "Seoul",
-    latitude: 37.5326,
-    longitude: 127.024612,
-    region: "asia-northeast3"
-  },
-  {
-    title: "Tokyo",
-    latitude: 35.652832,
-    longitude: 139.839478,
-    region: "asia-northeast1"
-  }
-];
-
-azure_loc = [
-  {
-    title: "Germany",
-    latitude: 50.110924,
-    longitude: 8.682127
-  },
-  {
-    title: "France",
-    latitude: 48.864716,
-    longitude: 2.349014
-  },
-  {
-    title: "Netherlands",
-    latitude: 52.314224,
-    longitude: 4.941841
-  },
-  {
-    title: "Finland",
-    latitude: 60.165249,
-    longitude: 24.936056
-  },
-  {
-    title: "UK",
-    latitude: 51.509865,
-    longitude: -0.118092
-  },
-  {
-    title: "Ireland",
-    latitude: 53.35014,
-    longitude: -6.266155
-  },
-  {
-    title: "Zurich",
-    latitude: 47.36667,
-    longitude: 8.55
-  },
-  {
-    title: "Oslo",
-    latitude: 59.911491,
-    longitude: 10.757933
-  },
-  {
-    title: "Sao Paulo",
-    latitude: -23.533773,
-    longitude: -46.62529
-  },
-  {
-    title: "Oregon",
-    latitude: 45.523064,
-    longitude: -122.676483
-  },
-  {
-    title: "Salt Lake City",
-    latitude: 40.746445,
-    longitude: -111.874916
-  },
-  {
-    title: "Los Angeles",
-    latitude: 34.052235,
-    longitude: -118.243683
-  },
-  {
-    title: "Iowa",
-    latitude: 41.619549,
-    longitude: -93.598022
-  },
-  {
-    title: "Canada",
-    latitude: 45.508888,
-    longitude: -73.561668
-  },
-  {
-    title: "North Verginia",
-    latitude: 39.01405,
-    longitude: -77.065674
-  },
-  {
-    title: "South Corolina",
-    latitude: 34.0003,
-    longitude: -81.0398
-  },
-  {
-    title: "Taiwan",
-    latitude: 25.105497,
-    longitude: 121.597366
-  },
-  {
-    title: "Mumbai",
-    latitude: 19.07609,
-    longitude: 72.877426
-  },
-  {
-    title: "Singapore",
-    latitude: 1.29027,
-    longitude: 103.851959
-  },
-  {
-    title: "Sydney",
-    latitude: -33.865143,
-    longitude: 151.2099
-  },
-  {
-    title: "Hong Kong",
-    latitude: 22.302219,
-    longitude: 114.174637
-  },
-  {
-    title: "Osaka",
-    latitude: 34.669529,
-    longitude: 135.497009
-  },
-  {
-    title: "Seoul",
-    latitude: 37.5326,
-    longitude: 127.024612
-  },
-  {
-    title: "Tokyo",
-    latitude: 35.652832,
-    longitude: 139.839478
-  }
-];
-
-module.exports.get_location = get_location;
+try {
+  module.exports.get_location = get_location;
+} catch (e) {}
